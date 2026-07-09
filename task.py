1b5cc62b16872669a99f119296d98a9d166733cb
gnijendra@gmail.com <gnijendra@gmail.com>
2026-07-08 21:46:20 -0400

    Task.py completed

--- task.py
+++ task.py
@@ -1,44 +1,42 @@
 class Task:
     def __init__(self, task_id, title, deadline, priority):
-        """
-        Initialize a Task.
-        
-        TODO: store all four+ attributes on self.
-        Remember: completed should default to False here — it's not a parameter,
-        because a brand-new task is never already done.
-        """
-        pass
+        self.task_id = task_id
+        self.title = title
+        self.deadline = deadline
+        self.priority = priority
+        self.completed = False
 
     def mark_complete(self):
-        """
-        TODO: flip self.completed to True.
-        Think about why this is a method on Task rather than something
-        Planner does by reaching into task.completed directly.
-        """
-        pass
+        self.completed = True
 
     def to_dict(self):
-        """
-        TODO: return this task as a plain dictionary.
-        Why do we need this? Because JSON doesn't know how to save a
-        custom Python object — it only understands dicts, lists, strings,
-        numbers, booleans. This method is the translator.
-        """
-        pass
+
+        return {"task_id": self.task_id,
+        "title" : self.title,
+        "deadline" : self.deadline,
+       "priority":  self.priority,
+        "completed": self.completed
+        }
 
     @staticmethod
     def from_dict(data):
-        """
-        TODO: given a dictionary (like the one to_dict produces),
-        build and return a Task object.
-        This is the reverse translator — dict back into object.
-        """
-        pass
+        task_id = data.get("task_id")
+        title = data.get("title")
+        deadline = data.get("deadline")
+        priority = data.get("priority")
+        
 
+        new_task = Task(task_id, title, deadline, priority)
+        new_task.completed = data.get("completed")
+
+        return new_task
+    
     def __str__(self):
-        """
-        TODO: return a human-readable string like:
-        "[✓] (1) Finish resume — due 2026-07-15 — HIGH"
-        This is what the user actually sees when tasks are listed.
-        """
-        pass
+        check = "✓" if self.completed else " "
+        return f"[{check}] ({self.task_id}) {self.title} - due {self.deadline} - {self.priority}"
+
+
+
+        
+
+
