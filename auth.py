c7096bef7a9a3be4ac0dd2d22aa82878008547e3
gnijendra@gmail.com <gnijendra@gmail.com>
2026-09-24 17:04:13 -0400

    Add Login function with email and bcrypt password verification

--- auth.py
+++ auth.py
@@ -7,3 +7,12 @@ def signup(user_id, email, plain_password):
     hashed_password = pwd_context.hash(plain_password)
     new_user = User(user_id, email, hashed_password)
     return new_user
+
+def login(email, plain_password, stored_user):
+    if email == stored_user.email:
+        if pwd_context.verify(plain_password, stored_user.hashed_password):
+            return "Login Successful"
+        else:
+            return "Login Failed"
+    else:
+        return "Login failed"
